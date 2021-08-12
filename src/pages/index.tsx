61fd15891d3227678d34ae5ee07f01d4f158e99d
Leonardo Alves <leoroberto.alves@hotmail.com>
2021-08-12 18:41:58 -0300

    Initial structure with Typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Home(): JSX.Element {
+  return (
+    <Head>
+      <title>Home | DevLeo</title>
+      <meta name="description" content="Devleo website" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
