2b547cfa6e4e63539dd4a219ab39f11548734df0
Leonardo Alves <leoroberto.alves@hotmail.com>
2021-08-12 18:53:46 -0300

    Configuration for ESlint and Prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 export default function Home(): JSX.Element {
-  return (
-    <Head>
-      <title>Home | DevLeo</title>
-      <meta name="description" content="Devleo website" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-  );
+   return (
+      <Head>
+         <title>Home | DevLeo</title>
+         <meta name="description" content="Devleo website" />
+         <link rel="icon" href="/favicon.ico" />
+      </Head>
+   )
 }
