158732a8c5d756e0c82c58d2b6c9f152337d549c
Leonardo Alves <leoroberto.alves@hotmail.com>
2021-08-12 19:14:09 -0300

    Configuration for Styled-components with themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import { AppProps } from 'next/app'
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyle from '../styles/global'
+
+import * as themes from '../styles/themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
-   return <Component {...pageProps} />
+   const [theme, setTheme] = useState(themes.dark)
+
+   return (
+      <ThemeProvider theme={theme}>
+         <Component {...pageProps} />
+         <GlobalStyle />
+      </ThemeProvider>
+   )
 }
 
 export default MyApp
